382778c63d05a59bcb0375c5638d1706826540df
Abhay5981 <abhaykum78@gmail.com>
2024-10-11 18:30:18 +0530

    api handle

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
+const quote = document.getElementById("quote")
+const author = document.getElementById("author")
+
+const quote_Url = "https://api.freeapi.app/api/v1/public/quotes/quote/random"
+async function generateQuote(url) {
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data);
+
+    if (data.data.content && data.data.author) {
+        quote.innerHTML = data.data.content;
+        author.innerHTML = data.data.author;
+    } else {
+        quote.innerHTML  = "Quote Not Found!"
+        author.innerHTML = "Undefined" 
+    }
+    
+}
+generateQuote(quote_Url);
+
+
+
 
 
